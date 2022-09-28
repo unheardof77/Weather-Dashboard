79b7e77ff0c4a22e41d4e3969abd81cadce9e813
Morgan Tolman <morgan.tolman04@gmail.com>
2022-09-27 18:51:41 -0700

    Setup local storage.  Got information to display again.

--- Assets/Devolpment/script.js
+++ Assets/Devolpment/script.js
@@ -1,6 +1,7 @@
+const $futureArea = document.getElementById(`futureForecast`);
 let historyList = document.getElementById(`historyList`);
 const searchButton = document.querySelector(`button`);
-
+let arrayOfHistory = JSON.parse(localStorage.getItem(`searchHistory`)) || []
 function getCoordinates(){
     cityName = document.querySelector(`input`).value.trim().replaceAll(` `, `-`);
     const cordAPI = `http://api.openweathermap.org/geo/1.0/direct?q=`+cityName+`&limit=1&appid=327e492de8c1e347e7f779666d577345`;
@@ -13,15 +14,18 @@ function getCoordinates(){
                 };
             })
         .then(function(data){
-            getFutureInfo(data);
+            getServerData(data);
         })
 };
 
 function historyButtonClick(event){
-    let clickedButton = event.target.getAttribute(`data-history`);
+    let clickedButton = event.target.getAttribute(`indexNumber`);
+    cityName = event.target.textContent;
+    
+    displayData(clickedButton);
 };
 
-function getFutureInfo(data){
+function getServerData(data){
     const weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat=`+data[0].lat+`&lon=`+data[0].lon+`&appid=327e492de8c1e347e7f779666d577345&units=imperial`;
     fetch(weatherURL)
         .then(function(response){
@@ -32,12 +36,56 @@ function getFutureInfo(data){
             }
         })
         .then(function(data){
-            displayData(data);
+            recentSearch = [{
+                currentTemp: `${data.current.temp}`,
+                currentWind: `${data.current.wind_speed}`,
+                humidity: `${data.current.humidity}`,
+                currentUvIndex: `${data.current.uvi}`,
+                currentImage:`${data.current.weather[0].icon}`
+            },]
+            for(i=0;i < 5 ;i++){
+                recentFutureSearch = {
+                    dailyImg: `${data.daily[i].weather[0].icon}`,
+                    dailyTemp: `${data.daily[i].temp.max}`,
+                    dailyWind: `${data.daily[i].wind_speed}`,
+                    dailyHumidity: `${data.daily[i].humidity}`
+                };
+                recentSearch.push(recentFutureSearch);
+            };
+            let firstSearch = arrayOfHistory.length  || 0
+            arrayOfHistory.push(recentSearch);
+            localStorage.setItem(`searchHistory`, JSON.stringify(arrayOfHistory));
+            storedHistory = JSON.parse(localStorage.getItem(`searchHistory`));
+            console.log(storedHistory)
+            displayData(firstSearch);
         })
 };
 
-function createFutureData(data){
-    const $futureArea = document.getElementById(`futureForecast`);
+function displayData(searchIndex){
+    let $temp = document.getElementById(`temp`);
+    let $wind = document.getElementById(`wind`);
+    let $humidity= document.getElementById(`humidity`);
+    let $uvIndex= document.getElementById(`uvIndex`);
+    let $titleH2 = document.getElementById(`cityTitle`);
+    let $currentImg = document.getElementById(`img`);
+    let $button = document.createElement(`button`);
+    $button.classList.add(`button`);
+    $button.setAttribute(`indexNumber`, searchIndex);
+    $button.textContent = cityName;
+    $currentImg.src = `./Assets/images/${storedHistory[searchIndex][0].currentImage}.png`;
+    $temp.textContent = storedHistory[searchIndex][0].currentTemp;
+    $wind.textContent = storedHistory[searchIndex][0].currentWind;
+    $humidity.textContent= storedHistory[searchIndex][0].humidity;
+    $uvIndex.textContent = storedHistory[searchIndex][0].currentUvIndex;
+    $titleH2.textContent = cityName;
+    historyList.appendChild($button);
+    $futureArea.innerHTML = ""
+    for(i=1;i < 6 ;i++){
+        createFutureData(searchIndex, i);
+    };
+};
+
+function createFutureData(searchIndex, i){
     const $futureSection = document.createElement(`section`);
     const $h3 = document.createElement(`h3`);
     const $ul = document.createElement(`ul`);
@@ -45,13 +93,13 @@ function createFutureData(data){
     const $windLi = document.createElement(`li`);
     const $humidityLi = document.createElement(`li`);
     const $dailyImg = document.createElement(`img`);
-    const UTC = data.daily[i].dt;
-    const $date = moment(UTC).local();
-    $dailyImg.src = `./Assets/images/${data.daily[i].weather[0].icon}.png`
-    $h3.textContent = $date
-    $tempLi.textContent =`Temp:` + data.daily[i].temp.max + `F`
-    $windLi.textContent =`Wind Speed:` + data.daily[i].wind_speed + `MPH`
-    $humidityLi.textContent =`Humidity:` + data.daily[i].humidity + `%`
+    //const UTC = data.daily[i].dt;
+    //const $date = moment(UTC).local();
+    //$h3.textContent = $date
+    $dailyImg.src = `./Assets/images/${storedHistory[searchIndex][i].dailyImg}.png`;
+    $tempLi.textContent =`Temp:${storedHistory[searchIndex][i].dailyTemp}F`;
+    $windLi.textContent =`Wind Speed:${storedHistory[searchIndex][i].dailyWind}MPH`;
+    $humidityLi.textContent =`Humidity:${storedHistory[searchIndex][i].dailyHumidity}%`;
     $futureSection.appendChild($h3);
     $futureSection.appendChild($dailyImg);
     $futureSection.appendChild($ul);
@@ -61,31 +109,6 @@ function createFutureData(data){
     $futureArea.appendChild($futureSection);
 };
 
-function displayData(data){
-    let $temp = document.getElementById(`temp`);
-    let $wind = document.getElementById(`wind`);
-    let $humidity= document.getElementById(`humidity`);
-    let $uvIndex= document.getElementById(`uvIndex`);
-    let $titleH2 = document.getElementById(`cityTitle`);
-    let $main = document.getElementById(`mainStats`);
-    let currentImg = document.createElement(`img`);
-    let currentIcon = data.current.weather[0].icon;
-    const $button = document.createElement(`button`);
-    $button.setAttribute(`history`, data.current.dt);
-    $button.classList.add(`button`);
-    $button.textContent = cityName;
-    currentImg.src = `./Assets/images/${currentIcon}.png`;
-    $main.appendChild(currentImg);
-    $temp.textContent = data.current.temp;
-    $wind.textContent = data.current.wind_speed;
-    $humidity.textContent= data.current.humidity;
-    $uvIndex.textContent = data.current.uvi;
-    $titleH2.textContent = cityName;
-    historyList.appendChild($button);
-    for(i=0;i < 5 ;i++){
-        createFutureData(data);
-    };
-};
 
 
 
